f0d2d7758022fe353675273d7237af790ba350d8
Samuel <a2jsamuel@gmail.com>
2023-11-01 22:25:21 +0300

    fix 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,14 @@ import logging
 from typing import List
 
 
+def filter_datum(fields, redaction, message, separator):
+    """returns the log message obfuscated"""
+    for fx in fields:  # This is ex: password={redaction}; OR password=xxx;
+        message = re.sub(f'{fx}=.+?{separator}',
+                         f'{fx}={redaction}{separator}', message)
+    return message
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -23,15 +31,8 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         """to filter values in incoming log records using filter_datum"""
         # rec = record.__dict__
-        filtered = filter_datum(self.fields, RedactingFormatter.REDACTION,
-                                record.getMessage(),
-                                RedactingFormatter.SEPARATOR)
+        record.msg = filter_datum(self.fields,
+                                      RedactingFormatter.REDACTION,
+                                      record.getMessage(),
+                                      RedactingFormatter.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
-
-
-def filter_datum(fields, redaction, message, separator):
-    """returns the log message obfuscated"""
-    for fx in fields:  # This is ex: password={redaction}; OR password=xxx;
-        message = re.sub(f'{fx}=.+?{separator}',
-                         f'{fx}={redaction}{separator}', message)
-    return message
