a0d80f8519cf4a2db8cbd47fe3ac7f96cbcd3de9
Samuel <a2jsamuel@gmail.com>
2023-11-01 22:26:41 +0300

    fix 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,8 +31,7 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         """to filter values in incoming log records using filter_datum"""
         # rec = record.__dict__
-        record.msg = filter_datum(self.fields,
-                                      RedactingFormatter.REDACTION,
-                                      record.getMessage(),
-                                      RedactingFormatter.SEPARATOR)
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(),
+                                  RedactingFormatter.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
