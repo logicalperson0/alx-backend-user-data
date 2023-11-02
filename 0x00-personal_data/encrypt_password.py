ecbc50cb1759a76e695814dfb98a0b6ee8a18a9c
Samuel <a2jsamuel@gmail.com>
2023-11-02 21:23:28 +0300

    add chk pwd with hashed_pwd func imp for task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,13 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """to validate that the provided password matches the hashed
+    password and return a boolean value"""
+    check_pwd = bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
+    if check_pwd:
+        return True
+    return False
