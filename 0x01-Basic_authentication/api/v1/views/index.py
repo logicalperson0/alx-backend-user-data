26413524f3e73b1136cf2816ee77cc924d4f0be6
Samuel <a2jsamuel@gmail.com>
2023-11-06 21:36:35 +0300

    for task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -34,3 +34,13 @@ def abort() -> str:
     """
     from api.v1.app import unauthorized
     return unauthorized(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden
+    Return:
+      raise a 403 error by using abort
+    """
+    from api.v1.app import forbidden
+    return forbidden(403)
