3863e74f6f3a17a8bc90aed04c9c9298ff8dabd1
Samuel <a2jsamuel@gmail.com>
2023-11-09 15:54:46 +0300

    This is for task 7

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Module of Users views
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request, make_response
+import os
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'],
+                 strict_slashes=False)
+def login() -> str:
+    """ POST /api/v1/auth_session/login
+    JSON body:
+      - email
+      - password
+    Return:
+      create a Session ID for the User ID
+    """
+    email = request.form.get('email')
+    # pwd = request.form.get('password')
+
+    if not email or email == '':
+        abort(jsonify({'error': 'email missing'}), 400)
+
+    pwd = request.form.get('password')
+    if not pwd or pwd == '':
+        abort(jsonify({'error': 'password missing'}), 400)
+
+    user_em = User.search({'email': email})
+    if user_em is None:
+        abort(jsonify({'error': 'no user found for this email'}), 404)
+
+    from api.v1.app import auth
+    for u in user_em:
+        if u.is_valid_password(pwd):
+            sess_id = auth.create_session(u.id)
+            SESSION_NAME = os.getenv('SESSION_NAME')
+            response = make_response(jsonify({}))
+            response.set_cookie(SESSION_NAME, sess_id)
+
+            return response
+
+    return abort(jsonify({'error': 'wrong password'}), 401)
