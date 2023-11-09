a999d4850f46ac7f3c4a7bfa1a858a0573cb6a30
Samuel <a2jsamuel@gmail.com>
2023-11-09 13:29:23 +0300

    this is for task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,3 +17,10 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[sess_id] = user_id
 
         return sess_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a User ID based on a Session ID"""
+
+        if session_id is None or type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
