d414ad12873e301240652c493a8e202f65d6b994
Samuel <a2jsamuel@gmail.com>
2023-11-16 18:28:42 +0300

    fix for task 9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,6 +52,6 @@ def _hash_password(password: str) -> bytes:
     return h_pwd
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """return a string representation of a new UUID"""
     return str(uuid.uuid4())
